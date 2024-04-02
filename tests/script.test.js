d0d1bda6dd9890302597c124ab6b434591f04f7f
stef359 <stefanossavv1@gmail.com>
2024-04-02 00:55:57 -0400

    Fixed import for a file

--- tests/script.test.js
+++ tests/script.test.js
@@ -1,6 +1,6 @@
 // tests/script.test.js
-import add from '../js/script'; // CommonJS import
+import add from "../src/script.js"; // CommonJS import
 
-test('adds 1 + 2 to equal 3', () => {
+test("adds 1 + 2 to equal 3", () => {
   expect(add(1, 2)).toBe(3);
 });
