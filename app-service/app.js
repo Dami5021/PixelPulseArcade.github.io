9f5db14f7ade78bde7756b1bb07de69470cd6494
stef359 <stefanossavv1@gmail.com>
2024-04-07 01:54:44 -0400

    Small changes in service

--- app-service/app.js
+++ app-service/app.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
-
 const app = express();
 
 app.use(bodyParser.json());
@@ -11,9 +10,13 @@ const corsOptions = {
   origin: "*",
   credentials: true,
 };
+app.get((req, res) => {
+  res.send("Welcome to App-Service, connection is succesful!"); // Send a simple response
+});
 
 app.use(cors(corsOptions));
 
+// Server initialization
 const port = 3500;
 const server = app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
