9675c7cd7679d282ee957e798dd490c81b14bb48
stef359 <stefanossavv1@gmail.com>
2024-04-10 01:01:26 -0400

    Added single comment

--- app-service/src/routes/UserRoutes.js
+++ app-service/src/routes/UserRoutes.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const userController = require("../controllers/UserController");
 
+// User routes
 router.get("/users", userController.getAllUsers);
 router.get("/users/:id", userController.getUser);
 router.post("/users", userController.addUser);
