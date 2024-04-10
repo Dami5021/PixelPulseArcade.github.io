f291f01913cac9b407671e5f756b33a270f9b92a
stef359 <stefanossavv1@gmail.com>
2024-04-09 22:34:15 -0400

    Establish connection to mongoDB

--- app-service/app.js
+++ app-service/app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+require("dotenv").config();
 const app = express();
 
 app.use(bodyParser.json());
@@ -10,14 +12,21 @@ const corsOptions = {
   origin: "*",
   credentials: true,
 };
-app.get((req, res) => {
-  res.send("Welcome to App-Service, connection is succesful!"); // Send a simple response
-});
+
+const DB_URI = process.env.DB_CONNECTION;
+mongoose
+  .connect(DB_URI)
+  .then(() => {
+    console.log("Connected to Mongo Database");
+  })
+  .catch((error) => {
+    console.error("Error connecting to MongoDB:", error);
+  });
 
 app.use(cors(corsOptions));
 
 // Server initialization
-const port = 3500;
-const server = app.listen(port, () => {
+const port = process.env.PORT || 3500;
+app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
