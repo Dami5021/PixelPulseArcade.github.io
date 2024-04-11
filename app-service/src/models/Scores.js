46527212335cd405f0634af45245bba1fe80e112
stef359 <stefanossavv1@gmail.com>
2024-04-11 15:32:20 -0400

    Add Date.now in Scores shema

--- app-service/src/models/Scores.js
+++ app-service/src/models/Scores.js
@@ -4,7 +4,7 @@ const ScoresSchema = new mongoose.Schema({
   username: { type: String, required: true },
   score: { type: Number, required: true },
   game: { type: String, required: true },
-  date: { type: Date, required: true },
+  date: { type: Date, default: Date.now(), required: true },
 });
 
 module.exports = mongoose.model("Scores", ScoresSchema);
