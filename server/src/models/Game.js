7dc7bf930b11a1b422b5373d1c761f4a82679b6c
stef359 <stefanossavv1@gmail.com>
2024-05-10 21:27:44 -0400

    Modify the Game Model

--- server/src/models/Game.js
+++ server/src/models/Game.js
@@ -2,7 +2,11 @@ const mongoose = require("mongoose");
 
 const GameSchema = new mongoose.Schema({
   name: { type: String, required: true },
+  genre: { type: String, required: true },
+  type: { type: String, required: true },
+  players: { type: String, required: true },
   description: { type: String, required: true },
+  imageSource: { type: String, required: true },
 });
 
 module.exports = mongoose.model("Game", GameSchema);
